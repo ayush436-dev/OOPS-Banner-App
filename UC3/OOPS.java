6ed189b219b2a4e74e83a94ec749db4f88df65ff
ayush436-dev <ayush4365144@gmail.com>
2026-03-18 13:50:37 +0530

    UC3 Code

---
+++ UC3/OOPS.java
@@ -0,0 +1,29 @@
+package UC3;
+
+public class OOPS {
+    
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+        " ***** ", " ***** ", " ****** ", " ***** "));
+
+        System.out.println(String.join(" ",
+        "*     *", "*     *", "*     *", "*     "));
+
+        System.out.println(String.join(" ",
+        "*     *", "*     *", "*     *", "*"));
+
+        System.out.println(String.join(" ",
+        "*     *", "*     *", "******", " ***** "));
+
+        System.out.println(String.join(" ",
+        "*     *", "*     *", "*", "            *"));
+
+        System.out.println(String.join(" ",
+        "*     *", "*     *", "*", "            *"));
+
+        System.out.println(String.join(" ",
+        " ***** ", " ***** ", "*",        "       ***** "));
+
+    }
+}
