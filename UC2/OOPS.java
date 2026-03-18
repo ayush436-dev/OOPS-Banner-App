1e2b1e37350c3f6f01af41f63742749ddda14ecd
ayush436-dev <ayush4365144@gmail.com>
2026-03-18 13:46:18 +0530

    UC2 Code

---
+++ UC2/OOPS.java
@@ -0,0 +1,16 @@
+package UC2;
+
+public class OOPS {
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   ******    ***** ");
+        System.out.println("*     * *     *  *     *  *      ");
+        System.out.println("*     * *     *  *     *  *      ");
+        System.out.println("*     * *     *  ******    ***** ");
+        System.out.println("*     * *     *  *              *");
+        System.out.println("*     * *     *  *              *");
+        System.out.println(" *****   *****   *         ***** ");
+
+    }
+}
+
